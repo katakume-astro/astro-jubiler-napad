3e17b10981273b7063fe093b8724e62532046bef
katakume <72819680+katakume-astro@users.noreply.github.com>
2022-12-03 19:32:41 +0100

    Update server.lua
    check lspd count

--- astro-heist/server.lua
+++ astro-heist/server.lua
@@ -1,4 +1,5 @@
 RegisterNetEvent("astro-heist:trollygift", function() AddItem(source, "money", 200) end)
+
 RegisterNetEvent("astro-heist:opendoor", function()
     local vangelico2 = exports.ox_doorlock:getDoor(93)
     local vangelico2 = exports.ox_doorlock:getDoorFromName('vangelico2')
@@ -6,9 +7,14 @@ RegisterNetEvent("astro-heist:opendoor", function()
 end)
 
 RegisterNetEvent("astro-heist:opendoors", function()
+    local count, workers = GetDataForJob("police")
+    if count > 2 then
     local vangelico = exports.ox_doorlock:getDoor(91)
     local vangelico = exports.ox_doorlock:getDoorFromName('vangelico')
     TriggerEvent('ox_doorlock:setState', vangelico.id, 0)
+           else
+            TriggerClientEvent('Astro-NotifySystem:Notify', source, 'error', 'Drzwi sie nie otworzyly, poniewaz nie ma lspd!')
+            end
 end)
 
 RegisterNetEvent("astro-heist:lockdoors", function()
